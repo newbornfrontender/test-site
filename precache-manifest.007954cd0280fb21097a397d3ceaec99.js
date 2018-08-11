9fab72cbe3650d93c6655632da112b8143a2de60
Infant Frontender <34810942+newbornfrontender@users.noreply.github.com>
2018-08-11 11:51:55 +0300

    Add files via upload

---
+++ precache-manifest.007954cd0280fb21097a397d3ceaec99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "37e5191b9eb9fe784c18",
+    "url": "/js/chunk-vendors.57bfdf91.js"
+  },
+  {
+    "revision": "e808b9e94f46d0dc2f58",
+    "url": "/js/app.bafc6809.js"
+  },
+  {
+    "revision": "260d945a8ff53ba43730",
+    "url": "/js/about.ecbc5746.js"
+  },
+  {
+    "revision": "2f5372791fca2742bc91739c003adea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e808b9e94f46d0dc2f58",
+    "url": "/css/app.aedbb21d.css"
+  }
+];
