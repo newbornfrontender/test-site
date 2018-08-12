5c2f32f6947340312f1bcae9769340b1b901e088
Infant Frontender <newbornfrontender@gmail.com>
2018-08-12 13:33:52 +0300

    Добавил новые файлы

---
+++ precache-manifest.d0e41c8dd27bbd77fc50b8fae0f7d546.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testsite/robots.txt"
+  },
+  {
+    "revision": "a6c11f9c969574a5351c",
+    "url": "/testsite/js/chunk-vendors.6b756030.js"
+  },
+  {
+    "revision": "4b42dfba753e34b0c6da",
+    "url": "/testsite/js/app.4f75bd83.js"
+  },
+  {
+    "revision": "260d945a8ff53ba43730",
+    "url": "/testsite/js/about.ecbc5746.js"
+  },
+  {
+    "revision": "65d2ea7313677c4ebf99de0fa034ddff",
+    "url": "/testsite/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/testsite/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4b42dfba753e34b0c6da",
+    "url": "/testsite/css/app.04cbb755.css"
+  }
+];
